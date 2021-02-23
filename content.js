dc0b5040d3a6aeb91138fbb1d72168f70cb296d1
Mendy Leizerovich <leizerovich.mendy@gmail.com>
2021-02-23 23:55:16 +0200

    Fix not match breadcrumb classes + change icon + add classes

--- content.js
+++ content.js
@@ -10,28 +10,23 @@ function htmlToElement(html) {
   return template.content.firstChild;
 }
 
-function generateCommitFileLink(domain, filePath) {
+/**
+ *
+ * @param {string} domain
+ * @param {string} filePath
+ * @param {string} linkClassList
+ * @param {string} svgClassList
+ */
+function generateCommitFileLink(domain, filePath, linkClassList, svgClassList) {
   return `
 <a
-  href="${domain.replace('tree', 'blob')}/${filePath}"
+  href="${domain}${filePath}"
   aria-label="See the file at this point in the history"
-  class="btn btn-outline tooltipped tooltipped-sw BtnGroup-item"
+  class="BtnGroup-item ${linkClassList}"
   rel="nofollow"
-  style="padding: 1.5px 5px;"
 >
-  <svg
-    fill="none"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    stroke-width="2"
-    stroke="currentColor"
-    viewBox="0 0 24 24"
-    width="24"
-    height="24"
-  >
-    <path
-      d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z"
-    ></path>
+  <svg class="${svgClassList}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+    <path fill-rule="evenodd" d="M4 1.75C4 .784 4.784 0 5.75 0h5.586c.464 0 .909.184 1.237.513l2.914 2.914c.329.328.513.773.513 1.237v8.586A1.75 1.75 0 0114.25 15h-9a.75.75 0 010-1.5h9a.25.25 0 00.25-.25V6h-2.75A1.75 1.75 0 0110 4.25V1.5H5.75a.25.25 0 00-.25.25v2.5a.75.75 0 01-1.5 0v-2.5zm7.5-.188V4.25c0 .138.112.25.25.25h2.688a.252.252 0 00-.011-.013l-2.914-2.914a.272.272 0 00-.013-.011zM5.72 6.72a.75.75 0 000 1.06l1.47 1.47-1.47 1.47a.75.75 0 101.06 1.06l2-2a.75.75 0 000-1.06l-2-2a.75.75 0 00-1.06 0zM3.28 7.78a.75.75 0 00-1.06-1.06l-2 2a.75.75 0 000 1.06l2 2a.75.75 0 001.06-1.06L1.81 9.25l1.47-1.47z"></path>
   </svg>
 </a>
 `;
@@ -39,25 +34,32 @@ function generateCommitFileLink(domain, filePath) {
 
 function run() {
   const breadcrumbs = [
-    ...document.querySelectorAll('.breadcrumb > span.js-path-segment'),
+    ...document.querySelectorAll('.breadcrumb > span:not(.js-repo-root)'),
   ]
-    .map(child => child.textContent)
-    .join('/');
-
+    .map((child) => child.textContent)
+    .join('');
   const file = document.querySelector('.breadcrumb strong')?.textContent;
 
   if (!breadcrumbs.length && !file?.length) {
     return;
   }
 
-  const filePath = `${breadcrumbs}/${file}`;
+  const filePath = `${breadcrumbs}${file}`;
   const browseRepositoryLinks = document.querySelectorAll(
     '.Box-row .d-none > a'
   );
 
-  browseRepositoryLinks.forEach(link => {
+  browseRepositoryLinks.forEach((link) => {
     const domain = link.href.replace('tree', 'blob');
-    const commitFileLink = generateCommitFileLink(domain, filePath);
+    const linkClasses = [...(link.classList ?? [])].join(' ');
+    const svg = link.querySelector('svg');
+    const svgClassList = [...(svg.classList ?? [])].join(' ');
+    const commitFileLink = generateCommitFileLink(
+      domain,
+      filePath,
+      linkClasses,
+      svgClassList
+    );
     const cloneLink = link.cloneNode(true);
 
     const linkToCommitFile = htmlToElement(commitFileLink);
